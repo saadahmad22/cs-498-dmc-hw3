88c5f869b8e7da7050ba942087fba2cc8ad76cac
Saad Ahmad <131712435+saadahmad22@users.noreply.github.com>
2025-04-17 20:41:46 -0500

    Update load_ev_data.py

--- load_ev_data.py
+++ load_ev_data.py
@@ -12,8 +12,8 @@ def load_data(csv_path):
     instance = client.instance(INSTANCE_ID)
     table = instance.table(TABLE_ID)
 
-    counter_row = table.direct_row(b"total_rows")
-    counter_row.set_cell("ev_info", b"count", str(0).encode())
+    counter_row = table.direct_row(b"total_rows", append=True)
+    counter_row.increment_cell_value(COLUMN_FAMILY_ID, b"count", 0)
     counter_row.commit()
 
     df = pd.read_csv(csv_path, dtype=str, na_filter=False)
@@ -22,11 +22,11 @@ def load_data(csv_path):
     for _, row in df.iterrows():
         row_key = row['DOL Vehicle ID'].encode()
         bt_row = table.direct_row(row_key)
-        bt_row.set_cell(COLUMN_FAMILY_ID, 'make', row['Make'])
+        bt_row.set_cell(COLUMN_FAMILY_ID, 'make', row['Make'].strip().upper())
         bt_row.set_cell(COLUMN_FAMILY_ID, 'model', row['Model'])
         bt_row.set_cell(COLUMN_FAMILY_ID, 'model year', row['Model Year'])
-        bt_row.set_cell(COLUMN_FAMILY_ID, 'electric range', row['Electric Range'].strip() if row['Electric Range'].strip() else "0")
-        bt_row.set_cell(COLUMN_FAMILY_ID, 'city', row['City'])
+        bt_row.set_cell(COLUMN_FAMILY_ID, 'electric range', row['Electric Range'].strip() or "0")
+        bt_row.set_cell(COLUMN_FAMILY_ID, 'city', row['City'].strip().upper())
         bt_row.set_cell(COLUMN_FAMILY_ID, 'county', row['County'])
         rows.append(bt_row)
         total_rows += 1
@@ -34,12 +34,14 @@ def load_data(csv_path):
         # batch write, then write rest
         if len(rows) == 500:
             table.mutate_rows(rows)
-            counter_row.increment_cell_value("ev_info", b"count", 500)
+            counter_row = table.row(b"total_rows", append=True)
+            counter_row.increment_cell_value(COLUMN_FAMILY_ID, b"count", 500)
             counter_row.commit()
             rows = []
     if rows:
         table.mutate_rows(rows)
-        counter_row.increment_cell_value("ev_info", b"count", len(rows))
+        counter_row = table.row(b"total_rows", append=True)
+        counter_row.increment_cell_value(COLUMN_FAMILY_ID, b"count", len(rows))
         counter_row.commit()
     print(f"Loaded {total_rows} rows.")
 
